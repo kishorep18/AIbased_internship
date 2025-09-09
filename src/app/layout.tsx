ca0fb4f6d31432786031af89c6b4010958183014
KISHORE P 2023-2027 <sec23it078@sairamtap.edu.in>
2025-09-09 05:03:55 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
-          crossOrigin="anonymous"
         />
       </head>
       <body className="font-body antialiased">
