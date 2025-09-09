09d480a4d86369f24504933412cbcd5fe6487ac4
KISHORE P 2023-2027 <sec23it078@sairamtap.edu.in>
2025-09-09 04:16:52 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
@@ -20,6 +21,7 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
+          crossOrigin="anonymous"
         />
       </head>
       <body className="font-body antialiased">
